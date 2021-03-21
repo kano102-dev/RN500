88f4f9fd9340b119aa119dbb312437aec4c3be0e
ranamehul20 <38745564+ranamehul20@users.noreply.github.com>
2021-03-21 14:36:48 +0530

    db changes

--- db/rn500.sql
+++ db/rn500.sql
@@ -151,5 +151,152 @@ CREATE TABLE `user_details` (
   CONSTRAINT `user_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+ALTER TABLE `user_details`
+ADD `profile_pic` varchar(250) NOT NULL AFTER `address_id`;
 
+ALTER TABLE `user_details`
+CHANGE `profile_pic` `profile_pic` varchar(250) COLLATE 'latin1_swedish_ci' NULL AFTER `address_id`;
+
+ALTER TABLE `user_details`
+ADD `current_position` varchar(250) COLLATE 'latin1_swedish_ci' NULL AFTER `profile_pic`,
+ADD `speciality` varchar(250) COLLATE 'latin1_swedish_ci' NULL AFTER `current_position`,
+ADD `work experience` varchar(250) COLLATE 'latin1_swedish_ci' NULL AFTER `speciality`,
+ADD `job_title` tinyint NULL COMMENT '(1) Actively Looking, (2) Looking from Date: MM/DD/YYYY.' AFTER `work experience`,
+ADD `job_looking_from` date NULL COMMENT 'required when job_title 2' AFTER `job_title`,
+ADD `travel_preference` tinyint NULL COMMENT '(1) 100%, (2) 50%, (3) 25% (3) 0% (4) Available anytime.' AFTER `job_looking_from`,
+ADD `ssn` int NULL COMMENT 'Last 4 Digit of SSN' AFTER `travel_preference`,
+ADD `work_authorization` tinyint NULL COMMENT '1:US Citizen ( ),2: Green Card Holder ( ),3: Other' AFTER `ssn`,
+ADD `work_authorization_comment` text COLLATE 'latin1_swedish_ci' NULL COMMENT 'required when work_authorization 3' AFTER `work_authorization`,
+ADD `license_suspended` text COLLATE 'latin1_swedish_ci' NULL AFTER `work_authorization_comment`,
+ADD `professional_liability` text COLLATE 'latin1_swedish_ci' NULL AFTER `license_suspended`;
+
+CREATE TABLE `job_location_preference` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `city` int NOT NULL,
+  `priority` int NOT NULL,
+   `user_id` int NOT NULL
+);
+
+CREATE TABLE `work_experience` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `organization_name` varchar(500) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+CREATE TABLE `education` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `degree_name` varchar(250) NOT NULL,
+  `year_complete` varchar(50) NOT NULL,
+  `institution` varchar(500) NOT NULL,
+  `location` varchar(500) NOT NULL
+);
+
+ALTER TABLE `education`
+ADD `user_id` int(11) NOT NULL AFTER `id`,
+ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+CREATE TABLE `certifications` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `certificate_name` varchar(250) NOT NULL,
+  `expiry_date` varchar(250) NOT NULL,
+  `issue_by` varchar(500) NOT NULL,
+  `verified` int NOT NULL,
+  `user_id` int(11) NOT NULL,
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+ALTER TABLE `certifications`
+ADD `issuing_state` int(11) NOT NULL AFTER `id`;
+
+ALTER TABLE `licenses`
+ADD `issuing_state` int(11) NOT NULL AFTER `id`;
+
+CREATE TABLE `references` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `first_name` varchar(250) NOT NULL,
+  `last_name` varchar(250) NOT NULL,
+  `mobile_no` varchar(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `city` int NOT NULL,
+  `state` int NOT NULL,
+  `relation` varchar(250) NOT NULL
+);
+
+ALTER TABLE `references`
+ADD `user_id` int(11) NOT NULL,
+ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+CREATE TABLE `skills` (
+  `id` int NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `expiry_date` varchar(250) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+CREATE TABLE `documents` (
+  `id` int NOT NULL,
+  `path` varchar(250) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+CREATE TABLE `dicipline` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(250) NOT NULL
+);
+
+ALTER TABLE `dicipline`
+CHANGE `name` `name` varchar(500) COLLATE 'latin1_swedish_ci' NOT NULL AFTER `id`;
+
+CREATE TABLE `benefits` (
+  `id` int NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `lead_id` int NOT NULL,
+  `cre` int NOT NULL
+);
+
+ALTER TABLE `benefits`
+CHANGE `cre` `created_at` int(11) NOT NULL AFTER `lead_id`,
+ADD `updated_at` int(11) NOT NULL;
+
+ALTER TABLE `dicipline`
+ADD `created_at` int NOT NULL,
+ADD `updated_at` int NOT NULL AFTER `created_at`,
+RENAME TO `discipline`;
+
+ALTER TABLE `benefits`
+DROP `lead_id`;
+
+CREATE TABLE `speciality` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(500) NOT NULL,
+  `created_at` int NOT NULL,
+  `updated_at` int NOT NULL
+);
+
+ALTER TABLE `benefits`
+CHANGE `id` `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
+
+CREATE TABLE `lead_discipline` (
+  `lead_id` int NOT NULL,
+  `discipline_id` int(11) NOT NULL,
+  FOREIGN KEY (`discipline_id`) REFERENCES `discipline` (`id`)
+);  
+
+CREATE TABLE `lead_benefit` (
+  `lead_id` int NOT NULL,
+  `benefit_id` int(11) NOT NULL,
+  FOREIGN KEY (`benefit_id`) REFERENCES `benefits` (`id`)
+);
+
+CREATE TABLE `lead_speciality` (
+  `lead_id` int NOT NULL,
+  `speciality_id` int(11) NOT NULL,
+  FOREIGN KEY (`speciality_id`) REFERENCES `speciality` (`id`)
+);
 -- 2021-03-21 06:43:23
