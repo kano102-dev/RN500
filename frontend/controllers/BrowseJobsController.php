b12812ab152ed30e52c3f75b2744403a7526642a
ranamehul20 <38745564+ranamehul20@users.noreply.github.com>
2021-04-23 12:25:52 +0530

    brows jobs dynamic

--- frontend/controllers/BrowseJobsController.php
+++ frontend/controllers/BrowseJobsController.php
@@ -49,38 +49,80 @@ class BrowseJobsController extends Controller {
     }
 
     public function actionIndex() {
-
-        return $this->render('index');
+        $query = LeadMaster::find();
+        $countQuery = clone $query;
+        $pages = new \yii\data\Pagination(['totalCount' => $countQuery->count()]);
+        $pages->setPageSize(10);
+        $models = $query->offset($pages->offset)->limit($pages->limit)->all();
+        return $this->render('index', ['models' => $models, 'pages' => $pages]);
     }
 
-    public function actionGetDiscipline($page = 0) {
+    public function actionGetDiscipline() {
+        $request = Yii::$app->getRequest()->post();
+        $page = isset($request['page']) ? $request['page'] : 0;
+        $filter = isset($request['filter']) && !empty($request['filter']) ? explode(',', $request['filter']) : [];
         $offset = isset($page) && !empty($page) ? $page : 0;
         $limit = 10;
         $totalRecord = Discipline::find()->count();
         $lists = ArrayHelper::map(Discipline::find()->limit($limit)->offset($offset)->all(), 'id', 'name');
         $options = "";
         foreach ($lists as $key => $list) {
-            $options .= "<li>
-                                <input type='checkbox' name='discipline' value='$key' id='desc-$key' />
-                                <label for='$key'></label>" . $list . " 
-                            </li>";
+            $options .= "<li>";
+            if (in_array($key, $filter)) {
+                $options .= "<input type='checkbox' name='discipline[]' value='$key' id='desc-$key' checked />";
+            } else {
+                $options .= "<input type='checkbox' name='discipline[]' value='$key' id='desc-$key' />";
+            }
+            $options .= "<label for='desc-$key'></label>" . $list;
+            $options .= "</li>";
         }
         $response = ['options' => $options, 'totalPage' => $totalRecord, 'offset' => count($lists)];
         echo Json::encode($response);
         exit;
     }
-    
-    public function actionGetSpecialty($page = 0) {
+
+    public function actionGetSpecialty() {
+        $request = Yii::$app->getRequest()->post();
+        $page = isset($request['page']) ? $request['page'] : 0;
+        $filter = isset($request['filter']) && !empty($request['filter']) ? explode(',', $request['filter']) : [];
         $offset = isset($page) && !empty($page) ? $page : 0;
         $limit = 10;
         $totalRecord = Speciality::find()->count();
         $lists = ArrayHelper::map(Speciality::find()->limit($limit)->offset($offset)->all(), 'id', 'name');
         $options = "";
         foreach ($lists as $key => $list) {
-            $options .= "<li>
-                                <input type='checkbox' name='speciality' value='$key' id='spec-$key' />
-                                <label for='$key'></label>" . $list . " 
-                            </li>";
+            $options .= "<li>";
+            if (in_array($key, $filter)) {
+                $options .= "<input type='checkbox' name='speciality[]' value='$key' id='spec-$key' checked />";
+            } else {
+                $options .= "<input type='checkbox' name='speciality[]' value='$key' id='spec-$key' />";
+            }
+            $options .= "<label for='spec-$key'></label>" . $list;
+            $options .= "</li>";
+        }
+        $response = ['options' => $options, 'totalPage' => $totalRecord, 'offset' => count($lists)];
+        echo Json::encode($response);
+        exit;
+    }
+
+    public function actionGetBenefits() {
+        $request = Yii::$app->getRequest()->post();
+        $page = isset($request['page']) ? $request['page'] : 0;
+        $filter = isset($request['filter']) && !empty($request['filter']) ? explode(',', $request['filter']) : [];
+        $offset = isset($page) && !empty($page) ? $page : 0;
+        $limit = 10;
+        $totalRecord = Benefits::find()->count();
+        $lists = ArrayHelper::map(Benefits::find()->limit($limit)->offset($offset)->all(), 'id', 'name');
+        $options = "";
+        foreach ($lists as $key => $list) {
+            $options .= "<li>";
+            if (in_array($key, $filter)) {
+                $options .= "<input type='checkbox' name='benefit[]' value='$key' id='benefit-$key' checked />";
+            } else {
+                $options .= "<input type='checkbox' name='benefit[]' value='$key' id='benefit-$key' />";
+            }
+            $options .= "<label for='benefit-$key'></label>" . $list;
+            $options .= "</li>";
         }
         $response = ['options' => $options, 'totalPage' => $totalRecord, 'offset' => count($lists)];
         echo Json::encode($response);
