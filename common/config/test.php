029bc527159b3ca032df84d6d4ae9ca9c1e526d7
ranamehul20 <38745564+ranamehul20@users.noreply.github.com>
2021-05-30 00:35:02 +0530

    recruiter HO owner doesn't show in staff recruiter owner role and employer owner role doesn't show anywhere except role listing  rename recruiter to recruiter company and employer to employer company in backend

--- common/config/test.php
+++ common/config/test.php
@@ -1,17 +1,17 @@
 <?php
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=rn500',
-//    'username' => 'root',
-//    'password' => 'root',
-//    'charset' => 'utf8',
-//];
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=170.10.161.93;dbname=rn500_dev',
-    'username' => 'rn500_dev',
-    'password' => 'welcome@12345',
+    'dsn' => 'mysql:host=localhost;dbname=rn500',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=170.10.161.93;dbname=rn500_dev',
+//    'username' => 'rn500_dev',
+//    'password' => 'welcome@12345',
+//    'charset' => 'utf8',
+//];
 
