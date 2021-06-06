9d08e01aeb5c023c091390f14a3d2d5943c51258
ranamehul20 <38745564+ranamehul20@users.noreply.github.com>
2021-06-06 10:26:45 +0530

    gitignore changes

--- common/config/main.php
+++ common/config/main.php
@@ -9,6 +9,27 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'db' => $db,
+        'socialShare' => [
+            'class' => \ymaker\social\share\configurators\Configurator::class,
+            'socialNetworks' => [
+                'Twitter' => [
+                    'class' => \ymaker\social\share\drivers\Twitter::class,
+                    'label' => \yii\helpers\Html::tag('i', '', ['class' => 'fa fa-twitter-square','style'=>"font-size: 27px;"]),
+                ],
+                'Gmail' => [
+                    'class' => \ymaker\social\share\drivers\Gmail::class,
+                    'label' => \yii\helpers\Html::tag('i', '', ['class' => 'fa fa-envelope','style'=>"font-size: 27px;"]),
+                ],
+                'LinkedIn' => [
+                    'class' => \ymaker\social\share\drivers\LinkedIn::class,
+                    'label' => \yii\helpers\Html::tag('i', '', ['class' => 'fa fa-linkedin-square','style'=>"font-size: 27px;"]),
+                ],
+                'Facebook' => [
+                    'class' => \ymaker\social\share\drivers\Facebook::class,
+                    'label' => \yii\helpers\Html::tag('i', '', ['class' => 'fa fa-facebook-square','style'=>"font-size: 27px;"]),
+                ],
+            ],
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'useFileTransport' => false,
